4fa104f9e4aa20b67c0d1e15569491b705b4e78e
Sergei Kozhevnikov <sergei.kozhevnikov@gmail.com>
2021-04-25 22:06:37 +0400

    change db from sqlite to postgres

--- backend/app/deps.py
+++ backend/app/deps.py
@@ -30,10 +30,10 @@ def get_current_user(
             token, settings.SECRET_KEY, algorithms=['HS256']
         )
         token_data = schemas.TokenPayload(**payload)
-    except (jwt.JWTError, ValidationError):
+    except (jwt.JWTError, ValidationError) as e:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
+            detail=f"Could not validate credentials",
         )
     user = crud.user.get(db, id=token_data.sub)
     if not user:
